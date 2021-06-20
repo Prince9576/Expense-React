fb02b2e21941d1c7db577959740f786de24af523
Prince Kumar <princek9576@gmail.com>
2021-06-20 23:57:18 +0530

    Added Expense Filter

--- src/App.js
+++ src/App.js
@@ -23,9 +23,12 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = (expenseData) => {
+    console.log("In app.js", expenseData);
+  };
   return (
     <div>
-      <NewExpense></NewExpense>
+      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
       <Expenses expenses={expenses}></Expenses>
     </div>
   );
